cc7649eb91b6903754257859e237f8a5e8567332
Meet Paija <mpaija@ibaset.com>
2019-03-04 10:39:23 +0530

    Allow cross origin in all APIs

--- src/main/java/com/project/OnlineLawSystemAPI/controller/CriminalController.java
+++ src/main/java/com/project/OnlineLawSystemAPI/controller/CriminalController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.project.OnlineLawSystemAPI.model.Criminal;
 import com.project.OnlineLawSystemAPI.service.ICriminalService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/criminal")
 public class CriminalController {
 	
